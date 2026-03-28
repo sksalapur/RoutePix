cd53856c7e14039eb8b2fb24c9493350fa669d21
sksalapur <sksalapur@gmail.com>
2026-03-29 00:49:09 +0530

    Bump version to v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.routepix"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "2.1.0"
+        versionCode = 16
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
